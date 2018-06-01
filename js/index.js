d50fc63489b962513615663795dc5bd210e1662c
Ryosuke Yamano <ryosuke.y.97@gmail.com>
2018-06-01 12:56:27 +0900

    fix: SP グロナビのアロー位置修正

--- js/index.js
+++ js/index.js
@@ -124,10 +124,23 @@ $(window).on('load',function(){
 			var POS = $(this).offset().top;
 			var scroll = $(window).scrollTop();
 			var windowHeight = $(window).height();
-			if (scroll > POS - windowHeight + windowHeight/5){
-				$(this).css("opacity","1" );
-			} else {
-				$(this).css("opacity","0" );
+			if (scroll > POS - windowHeight){
+				$(this).css({
+					'opacity':'1',
+					'transform':'translateY(0)',
+					'-webkit-transform':'translateY(0)',
+					'-moz-transform':'translateY(0)',
+					'-ms-transform':'translateY(0)'
+				});
+			}
+			else {
+				$(this).css({
+					'opacity':'0',
+					'transform':'translateY(70px)',
+					'-webkit-transform':'translateY(70px)',
+					'-moz-transform':'translateY(70px)',
+					'-ms-transform':'translateY(70px)'
+				});
 			}
 		});
 	});
